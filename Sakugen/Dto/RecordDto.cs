5949957736fc73d6a8b467e6bdd8b515f1ceeb58
JustKaneri <justkaneri@yandex.ru>
2023-05-27 19:44:47 +0300

    Change icon

--- Sakugen/Dto/RecordDto.cs
+++ Sakugen/Dto/RecordDto.cs
@@ -6,7 +6,8 @@ namespace Sakugen.Dto
     {
         public int Id { get; set; }
         public string Token { get; set; }
-        [RegularExpression(@"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)", ErrorMessage = "Некорректный адрес")]
+
+        [Url(ErrorMessage = "Некорректный адрес")]
         public string? Url { get; set; }
         public byte[] QrCode { get; set; }
     }
--- Sakugen/Models/Record.cs
+++ Sakugen/Models/Record.cs
@@ -16,7 +16,7 @@ namespace Sakugen.Models
 
         [Column("Url")]
         [MaxLength(int.MaxValue)]
-        [RegularExpression(@"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)", ErrorMessage = "Некорректный адрес")]
+        [Url]
         public string? Url { get; set; }
 
         [Column("DateCreate")]
