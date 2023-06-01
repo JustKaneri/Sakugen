1e280d25efb43e11810e7666c25fea69cbc20d86
JustKaneri <justkaneri@yandex.ru>
2023-06-01 19:53:50 +0300

    Create unit test poject

---
+++ SakugenTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sakugen.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakugen.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        private readonly HomeController _controller;
+        private ViewResult _result;
+
+        public HomeControllerTests(HomeController controller)
+        {
+            _controller = controller;
+        }
+
+        [TestInitialize]
+        public void SetupContext()
+        {
+            _result = _controller.Index() as ViewResult;
+        }
+
+        [TestMethod()]
+        public void IndexViewResultNotNull()
+        {
+            Assert.IsNotNull(_result);
+        }
+    }
+}
